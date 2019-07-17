9e8745ae571c3077401ef3e556e084bade73c91d
Dylan <dylan.vanwaarden@jedlix.com>
2019-07-17 21:03:10 +0200

    Change to correct data format

--- web/networks.py
+++ web/networks.py
@@ -2,5 +2,8 @@ from wifi import Cell, Scheme
 
 def getNetworks():
     networks = list(Cell.all('wlan0'))
-    ssids = list(map(lambda x: x.ssid, networks))
+    ssids = list(set(list(map(lambda x: x.ssid, networks))))
+    finalnetworks = []
+    for ssid in ssids:
+        finalnetworks.append((ssid, ssid))
     return ssids
