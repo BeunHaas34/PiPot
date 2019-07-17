834d1b051e531eb199f344455bf2517622857290
Dylan <dylan.vanwaarden@jedlix.com>
2019-07-17 21:21:16 +0200

    Test

--- web/networks.py
+++ web/networks.py
@@ -1,10 +1,21 @@
 from wifi import Cell, Scheme
 
+
 def getNetworks():
-    networks = list(Cell.all('wlan0'))
+    #networks = list(Cell.all('wlan0'))
+    networks = test()
     ssids = list(map(lambda x: x.ssid, networks))
     finalnetworks = []
     for ssid in ssids:
-        finalnetworks.append((ssid, ssid))
+        finalnetworks.append((ssid, ssids.index(ssid)))
+    ssids = [("1", 1), ("2", 2)]
     print(ssids)
     return ssids
+
+
+def test():
+    a = Cell()
+    a.ssid = "networkA"
+    b = Cell()
+    b.ssid = "networkB"
+    return [a, b]
