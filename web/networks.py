db32fec8dde65560cc014852647e857a24b3dd50
Dylan <dylan.vanwaarden@jedlix.com>
2019-07-17 21:04:41 +0200

    Trying to remove duplicates does not work

--- web/networks.py
+++ web/networks.py
@@ -2,7 +2,7 @@ from wifi import Cell, Scheme
 
 def getNetworks():
     networks = list(Cell.all('wlan0'))
-    ssids = list(set(list(map(lambda x: x.ssid, networks))))
+    ssids = list(map(lambda x: x.ssid, networks))
     finalnetworks = []
     for ssid in ssids:
         finalnetworks.append((ssid, ssid))
