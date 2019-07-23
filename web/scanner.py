c809c71e1bc33843752cc2d49112642cf8a7bfb3
Dylan <dylan.vanwaarden@jedlix.com>
2019-07-23 22:08:00 +0200

    Try different send

--- web/scanner.py
+++ web/scanner.py
@@ -30,5 +30,5 @@ def check(message):
             mac = mac_search.group(2)
 
             channel_layer = get_channel_layer()
-            async_to_sync(channel_layer.group_send)('stream', {'message': mac})
+            async_to_sync(channel_layer.send)('stream', {'message': mac})
             print(mac)
