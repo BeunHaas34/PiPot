109b901454dda0608c013ebd913ba8419d5b42e7
Mike Nachtigaal <mnachtigaal@outlook.com>
2019-08-04 19:10:01 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Disable IPV, due to bug with pip
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo sysctl -p
+
+# Check if Python3 is installed
+if ! hash python3 && ! hash pip3 ; then
+    echo "Installing Python3"
+    sudo apt-get update
+    sudo apt-get install python3 python3-pip
+else
+    echo "Python3 is installed"
+fi
+
+# Check is Hostapd is installed
+
+if [ ! -f "./hostapd-mana/hostapd/hostapd" ] ; then
+    echo "Installing hostapd"
+    sudo apt-get install build-essential libssl-dev libnl-dev libnl-genl-3-dev libssl1.0-dev -y
+    git clone https://github.com/sensepost/hostapd-mana
+    cd hostapd-mana
+    make -C hostapd
+else
+    echo "Hostapd is installed"
+fi
+
+# Check if PiPot is installed
+if [ ! -d "PiPot" ]; then
+    echo "Installing PiPot"
+    # TODO Change branch
+    git clone https://github.com/TheHell34/PiPot.git -b develop PiPot
+    cd PiPot
+    echo "Creating venv"
+    python3 -m venv venv
+    echo "pip install"
+    sudo ./venv/bin/pip3 install -r requirements.txt
+else
+    echo "PiPot is installed"
+    cd PiPot
+fi
+
+sudo python3 manage.py runserver
+
+
